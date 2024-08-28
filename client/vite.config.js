330421c6125f4df506586a30ce1440d0b59fa4aa
codelformat <codelformat@gmail.com>
2024-08-28 12:03:33 +0800

    add signup functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      // String starts with '/api' will be handled by the proxy 
+      // Example: /api/auth/signup -> http://localhost:3000/api/auth/signup
+      '/api':{
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
