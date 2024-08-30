f71d406e5f9ceb1b058efb7f539e3d397afc6626
QWERTY0205 <wansb58@gmail.com>
2024-08-30 15:33:34 +0800

    Change profile picture without uploading

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,17 +1,54 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { Button, TextInput } from "flowbite-react";
+import { useState, React, useRef, useEffect } from "react";
+import { set } from "mongoose";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  //上传profiePicture
+  const [imageFile, setImageFile] = useState(null);
+  //不直接传图片 而是给出地址
+  const [imageFileURL, setImageURL] = useState(null);
+  //转换链接 把对fileInput的引用传给filePickerRef
+  const filePickerRef = useRef(null);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageURL(URL.createObjectURL(file));
+    }
+  };
+  //后端上传图片
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+  const uploadImage = async () => { 
+    console.log("uploading image");
+    //需要
+  }
+
+
   //currentUser里面有rest 要用 currentUser.rest.username
+  //数据库里没有profilePicture 有profiePicture
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
           <img
-            src={currentUser.rest.profiePicture}
+            src={imageFileURL || currentUser.rest.profiePicture}
             alt="user"
             className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
           ></img>
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -24,12 +24,13 @@ export default function DashSidebar() {
               active={tab === "profile"}
               icon={HiUser}
               label={"User"}
-              lableColor="dark"
+              lablecolor="dark"
+              as="button"
             >
               Profile
             </Sidebar.Item>
           </Link>
-          <Sidebar.Item icon={HiArrowSmRight} classname="cursor-pointer">
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
