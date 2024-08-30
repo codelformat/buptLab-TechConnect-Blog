6227cfed64afe64ce974f7aad0f21a2dd6f8db9f
QWERTY0205 <wansb58@gmail.com>
2024-08-30 13:13:18 +0800

    profile UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Button, TextInput } from "flowbite-react";
 
 export default function DashProfile() {
+  const { currentUser } = useSelector((state) => state.user);
+  //currentUser里面有rest 要用 currentUser.rest.username
   return (
-    <div>DashProfile</div>
-  )
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img
+            src={currentUser.rest.profiePicture}
+            alt="user"
+            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+          ></img>
+        </div>
+        <TextInput
+          type="text "
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.rest.username}
+        ></TextInput>
+        <TextInput
+          type="email "
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.rest.email}
+        ></TextInput>
+        <TextInput
+          type="password "
+          id="password"
+          placeholder="password"
+          defaultValue="*********"
+        ></TextInput>
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
+        <div className="text-red-500 flex justify-between mt-5">
+          <span className="cursor-pointer">Delete Account</span>
+          <span className="cursor-pointer">Sign Out</span>
+        </div>
+      </form>
+    </div>
+  );
 }
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Outlet,Navigate } from 'react-router-dom'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
 export default function privateRoute() {
-    //从state中解构出currentUser 判断是否为空 进行重定向
-    const{currentUser}=useSelector(state=>state.user)
-    return (
-        currentUser ? <Outlet /> : <Navigate to="/sign-in" />
-    );
+  //从state中解构出currentUser 判断是否为空 进行重定向
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
