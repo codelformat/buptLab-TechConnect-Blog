4ee82355174a4486f634cd55bd82fde8605e0ce2
codelformat <codelformat@gmail.com>
2024-09-01 15:46:04 +0800

    refactor(after signout)

---
+++ client/src/components/widgets/AccountActions.jsx
@@ -0,0 +1,72 @@
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { Modal, Button } from "flowbite-react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { deleteUserStart, deleteUserFailure, deleteUserSuccess, signoutSuccess } from "../../redux/user/userSlice";
+
+export default function AccountActions({ currentUser }) {
+  const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleDeleteUser = async () => {
+    setShowModal(false);
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser.rest ? currentUser.rest._id : currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(deleteUserFailure(data.message));
+      } else {
+        dispatch(deleteUserSuccess());
+      }
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="text-red-500 flex justify-between mt-5">
+      <span className="cursor-pointer" onClick={() => setShowModal(true)}>
+        Delete Account
+      </span>
+      <span onClick={handleSignout} className="cursor-pointer">Sign Out</span>
+      <Modal show={showModal} onClose={() => setShowModal(false)} popup size="md">
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="h-14 w-14 text-5xl text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+              Are you sure to delete your account?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={handleDeleteUser}>
+                Yes, I'm sure
+              </Button>
+              <Button color="gray" onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
