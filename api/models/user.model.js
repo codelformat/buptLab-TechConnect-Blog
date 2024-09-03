a9dbcf63f8e40b55839bce03333cdf02f4f5ea52
codelformat <codelformat@gmail.com>
2024-09-03 18:09:58 +0800

    add forgot&reset password, from gmail

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -25,6 +25,8 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    resetPasswordCode: String,
+    resetPasswordExpire: Date,
     }, {timestamps: true}
 );
 
