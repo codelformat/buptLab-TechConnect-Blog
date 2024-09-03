27acead095155d15b25cc659b0813d6580b5d4c1
codelformat <codelformat@gmail.com>
2024-09-03 14:56:47 +0800

    refactor footer

---
+++ client/src/components/widgets/FooterLinksGroup.jsx
@@ -0,0 +1,21 @@
+import { Footer } from "flowbite-react";
+
+export default function FooterLinksGroup({ title, links }) {
+  return (
+    <div>
+      <Footer.Title title={title} />
+      <Footer.LinkGroup col>
+        {links.map((link, index) => (
+          <Footer.Link
+            key={index}
+            href={link.href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {link.label}
+          </Footer.Link>
+        ))}
+      </Footer.LinkGroup>
+    </div>
+  );
+}
