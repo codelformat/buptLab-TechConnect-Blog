a09532e948d70707b306d63d5faef272efebbba2
kikoloki <121781321+kikoloki@users.noreply.github.com>
2024-09-03 14:42:48 +0800

    home

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,61 @@
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center p-4">
+      {/* The main card container */}
+      <div className="w-full max-w-4xl flex flex-col md:flex-row bg-white rounded-lg shadow-lg overflow-hidden">
+
+        {/* Left side with contact information and gradient background */}
+        <div className="p-8 md:w-1/3 w-full glowing-gradient flex flex-col justify-between text-white">
+          <div>
+            <h2 className="text-2xl font-semibold mb-4">Contact Information</h2>
+            <p className="text-md mb-8">Say something to start a live chat!</p>
+            <div className="space-y-4">
+              <div className="flex items-center gap-3">
+                <i className="fas fa-phone-alt"></i>
+                <span>+1012 3456 789</span>
+              </div>
+              <div className="flex items-center gap-3">
+                <i className="fas fa-envelope"></i>
+                <span>demo@gmail.com</span>
+              </div>
+              <div className="flex items-center gap-3">
+                <i className="fas fa-map-marker-alt"></i>
+                <span>132 Dartmouth Street Boston, Massachusetts 02156 United States</span>
+              </div>
+            </div>
+          </div>
+          <div className="flex space-x-4 mt-8">
+            <i className="fab fa-twitter"></i>
+            <i className="fab fa-instagram"></i>
+            <i className="fab fa-discord"></i>
+          </div>
+        </div>
+
+        {/* Right side with blog information */}
+        <div className="p-8 md:w-2/3 w-full flex flex-col justify-center border-l-2 border-gray-300 shadow-lg">
+          <h1 className="text-3xl font-semibold text-center mb-7">About Sahand's Blog</h1>
+          <div className="text-md text-gray-700 space-y-6">
+            <p>
+              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
+              as a personal project to share his thoughts and ideas with the
+              world. Sahand is a passionate developer who loves to write about
+              technology, coding, and everything in between.
+            </p>
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. Sahand is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,165 @@
+import { Button, Select, TextInput } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import PostCard from '../components/PostCard';
+
+export default function Search() {
+  const [sidebarData, setSidebarData] = useState({
+    searchTerm: '',
+    sort: 'desc',
+    category: 'uncategorized',
+  });
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showMore, setShowMore] = useState(false);
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    const sortFromUrl = urlParams.get('sort');
+    const categoryFromUrl = urlParams.get('category');
+    if (searchTermFromUrl || sortFromUrl || categoryFromUrl) {
+      setSidebarData({
+        ...sidebarData,
+        searchTerm: searchTermFromUrl,
+        sort: sortFromUrl,
+        category: categoryFromUrl,
+      });
+    }
+
+    const fetchPosts = async () => {
+      setLoading(true);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/post/getposts?${searchQuery}`);
+      if (!res.ok) {
+        setLoading(false);
+        return;
+      }
+      const data = await res.json();
+      setPosts(data.posts);
+      setLoading(false);
+      setShowMore(data.posts.length === 9);
+    };
+    fetchPosts();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setSidebarData((prevData) => ({
+      ...prevData,
+      [id]: value || (id === 'sort' ? 'desc' : 'uncategorized'),
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams({
+      searchTerm: sidebarData.searchTerm,
+      sort: sidebarData.sort,
+      category: sidebarData.category,
+    });
+    navigate(`/search?${urlParams.toString()}`);
+  };
+
+  const handleShowMore = async () => {
+    const startIndex = posts.length;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/post/getposts?${searchQuery}`);
+    if (res.ok) {
+      const data = await res.json();
+      setPosts((prevPosts) => [...prevPosts, ...data.posts]);
+      setShowMore(data.posts.length === 9);
+    }
+  };
+
+  return (
+    <div className="relative flex">
+      {/* 左侧的隐藏菜单 */}
+      <div
+        className={`fixed top-0 left-0 h-full w-64 bg-white shadow-lg p-7 border-r border-gray-500 transition-transform transform ${
+          isSidebarVisible ? 'translate-x-0' : '-translate-x-full'
+        } md:border-r`}
+        onMouseLeave={() => setSidebarVisible(false)}
+      >
+        <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+          <div className="flex items-center gap-2">
+            <label className="whitespace-nowrap font-semibold">
+              Search Term:
+            </label>
+            <TextInput
+              placeholder="Search..."
+              id="searchTerm"
+              type="text"
+              value={sidebarData.searchTerm}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <label className="font-semibold">Sort:</label>
+            <Select
+              onChange={handleChange}
+              value={sidebarData.sort}
+              id="sort"
+            >
+              <option value="desc">Latest</option>
+              <option value="asc">Oldest</option>
+            </Select>
+          </div>
+          <div className="flex items-center gap-2">
+            <label className="font-semibold">Category:</label>
+            <Select
+              onChange={handleChange}
+              value={sidebarData.category}
+              id="category"
+            >
+              <option value="uncategorized">Uncategorized</option>
+              <option value="reactjs">React.js</option>
+              <option value="nextjs">Next.js</option>
+              <option value="javascript">JavaScript</option>
+            </Select>
+          </div>
+          <Button type="submit" outline gradientDuoTone="purpleToPink">
+            Apply Filters
+          </Button>
+        </form>
+      </div>
+
+      {/* 用于触发显示菜单的区域 */}
+      <div
+        className="fixed top-0 left-0 h-full w-2 md:w-4 bg-transparent cursor-pointer"
+        onMouseEnter={() => setSidebarVisible(true)}
+      ></div>
+
+      {/* 右侧的内容区域 */}
+      <div className="flex-1">
+        <h1 className="text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5">
+          Posts results:
+        </h1>
+        <div className="p-7 flex flex-wrap gap-4">
+          {!loading && posts.length === 0 && (
+            <p className="text-xl text-gray-500">No posts found.</p>
+          )}
+          {loading && <p className="text-xl text-gray-500">Loading...</p>}
+          {!loading &&
+            posts &&
+            posts.map((post) => <PostCard key={post._id} post={post} />)}
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="text-teal-500 text-lg hover:underline p-7 w-full"
+            >
+              Show More
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
