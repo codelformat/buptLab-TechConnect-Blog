c74bd10e45f56ce09d7b2c8177d1548cdedb4a59
codelformat <codelformat@gmail.com>
2024-09-04 14:36:49 +0800

    fix onlyadmin rest

--- client/src/components/OnlyAdminPrivateRoute.jsx
+++ client/src/components/OnlyAdminPrivateRoute.jsx
@@ -3,8 +3,13 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 export default function OnlyAdminPrivateRoute() {
   // const { currentUser } = useSelector((state) => state.user);
+  let currentUser = null;
   const tempUser = useSelector((state) => state.user);
-  const currentUser = tempUser.currentUser.rest? tempUser.currentUser.rest : tempUser.currentUser;
+  if (tempUser.currentUser){
+    currentUser = tempUser.currentUser.rest? tempUser.currentUser.rest : tempUser.currentUser;
+  } else{
+    currentUser = null
+  }
   return currentUser && currentUser.isAdmin ? (
     <Outlet />
   ) : (
